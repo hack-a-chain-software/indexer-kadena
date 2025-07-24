1ae4d6ea75e3f4afbb36cea4e57faac0c131351c
0xneves <asguilift@hotmail.com>
2025-07-24 17:26:52 -0400

    fix: not using sentry if var is not provided

--- indexer/src/kadena-server/plugins/instrument.ts
+++ indexer/src/kadena-server/plugins/instrument.ts
@@ -9,24 +9,25 @@ const SENTRY_DSN = process.env['SENTRY_DSN'];
 const SENTRY_ENVIRONMENT = 'production';
 const SENTRY_RELEASE = 'v1.0.0';
 
-if (SENTRY_DSN === undefined || SENTRY_DSN === 0) {
-  console.info('[INFO][SENTRY][LOG] Not using Sentry as it is disabled by not setting SENTRY_DSN env var');
-  return; 
+if (!SENTRY_DSN || SENTRY_DSN.trim() === '') {
+  console.info(
+    '[INFO][SENTRY][LOG] Not using Sentry as it is disabled by not setting SENTRY_DSN env var',
+  );
+} else {
+  // Initialize Sentry
+  console.info('[INFO][SENTRY][INIT] Initializing Sentry error reporting');
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    environment: SENTRY_ENVIRONMENT,
+    release: SENTRY_RELEASE,
+    sendDefaultPii: true,
+    tracesSampleRate: 1.0,
+    profileSessionSampleRate: 1.0,
+    profileLifecycle: 'trace',
+    integrations: [
+      nodeProfilingIntegration(),
+      Sentry.graphqlIntegration({ useOperationNameForRootSpan: true }),
+    ],
+    debug: false,
+  });
 }
-
-// Initialize Sentry
-console.info('[INFO][SENTRY][INIT] Initializing Sentry error reporting');
-Sentry.init({
-  dsn: SENTRY_DSN,
-  environment: SENTRY_ENVIRONMENT,
-  release: SENTRY_RELEASE,
-  sendDefaultPii: true,
-  tracesSampleRate: 1.0,
-  profileSessionSampleRate: 1.0,
-  profileLifecycle: 'trace',
-  integrations: [
-    nodeProfilingIntegration(),
-    Sentry.graphqlIntegration({ useOperationNameForRootSpan: true }),
-  ],
-  debug: false,
-});
